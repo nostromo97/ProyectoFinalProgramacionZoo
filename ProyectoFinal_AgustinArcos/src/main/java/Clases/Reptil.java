b4a7a259306394041922cf1f6c8b3d755cb6cf4d
nostromo97 <chesopan@gmail.com>
2022-06-10 20:02:01 +0200

    documentacion clases acabada
    documentacion clases acabada

--- ProyectoFinal_AgustinArcos/src/main/java/Clases/Reptil.java
+++ ProyectoFinal_AgustinArcos/src/main/java/Clases/Reptil.java
@@ -10,19 +10,47 @@ import Enums.MotivoAlta;
 import Enums.MotivoBaja;
 import Enums.TipoPiel;
 import Enums.TipoRaza;
+import Excepciones.CampoVacioException;
 import Excepciones.DescripcionVaciaException;
 import Excepciones.FechaFormatoException;
+import Excepciones.GeneroVacioException;
 import Excepciones.IdInvalidoException;
 import Excepciones.IdVacioException;
 import Excepciones.MotivoVacioException;
 import Excepciones.NombreInvalidoException;
 import Excepciones.NombreVacioException;
 import Utils.UtilsDB;
-
+/**
+ * Clase que contiene un objeto de tipo Reptil que extiende de la clase Animal.
+ * @author Agustín Arcos
+ *
+ */
 public class Reptil extends Animal{
-
+	/**
+	 * Variable de tipo boolean que indica el tipo de piel del reptil, escamas o caparazaón, siendo los valores true o false.
+	 */
 	private boolean tipoPiel;
-
+/**
+ * Constructor de Reptil.
+ * 
+ * @param id Variable numérica de tipo short que contiene el id del animal. 
+ * @param fechaNacimiento Variable de tipo LocalDate que contiene la fecha de nacimiento del animal.
+ * @param nombre Cadena de caracteres que contiene el nombre del animal.
+ * @param raza Proveniente del Enum TipoRaza que contiene el tipo de raza del animal.
+ * @param cuidados Cadena de caracteres que contiene una descripción del cuidado 
+ * 				   necesario para el animal, en el caso de que lo necesite.
+ * @param motivoAlta Proviene del Enum MotivoAlta que contiene los motivos de alta del animal.
+ * @param motivoBaja Proviene del Enum MotivoBaja que contiene los motivos de baja del animal.
+ * @param fechaAlta De tipo LocalDate, contiene la fecha de alta del animal en el centro.
+ * @param fechaBaja De tipo LocalDate, contiene la fecha de baja del animal en el centro.
+ * @param dieta De tipo Dieta, contiene ArrayList cada día de la semana con los alimentos que come al día el animal.
+ * @param tipoPiel Variable de tipo boolean que indica el tipo de piel del reptil, escamas o caparazaón, siendo los valores true o false.
+ * @throws NombreVacioException Excepción que se lanza cuando el nombre está vacío. 
+ * @throws NombreInvalidoException Excepción que se lanza cuando el nombre no es válido.
+ * @throws IdInvalidoException Excepción quqe se lanza cuando la id no es válida.
+ * @throws IdVacioException Excepción que se lanza cuando el id está vacío.
+ * @throws FechaFormatoException Excepción que se lanza cuando la fecha se introduce en un formato incorrecto.
+ */
 	public Reptil(short id, LocalDate fechaNacimiento, String nombre, TipoRaza raza, String cuidados,
 			MotivoAlta motivoAlta, MotivoBaja motivoBaja, LocalDate fechaAlta, LocalDate fechaBaja, Dieta dieta,
 			boolean tipoPiel) throws NombreVacioException, NombreInvalidoException, IdInvalidoException, IdVacioException, FechaFormatoException {
@@ -30,7 +58,28 @@ public class Reptil extends Animal{
 		this.tipoPiel = tipoPiel;
 	}
 
-	//CONSTRUCTOR ALTA
+	/**
+	 * 	Constructor de la clase Reptil que extiende de Animal, utilizado para dar de alta a un Reptil.
+	 * 
+	 * @param nombre Cadena de caracteres que contiene el nombre del animal.                         
+	 * @param fechaNacimiento Variable de tipo LocalDate que contiene la fecha de nacimiento del animal.
+	 * @param motivoAlta Proviene del Enum MotivoAlta que contiene los motivos de alta del animal.
+	 * @param fechaAlta De tipo LocalDate, contiene la fecha de alta del animal en el centro.
+	 * @param cuidados Cadena de caracteres que contiene una descripción del cuidado 
+	 * 				   necesario para el animal, en el caso de que lo necesite.
+	 * @param tipoPiel Variable de tipo boolean que indica el tipo de piel del reptil, escamas o caparazaón, siendo los valores true o false.                  
+	 * @throws NombreVacioException Excepción que se lanza cuando el nombre está vacío. 
+	 * @throws NombreInvalidoException Excepción que se lanza cuando el nombre no es válido.
+	 * @throws IdInvalidoException Excepción quqe se lanza cuando la id no es válida.
+	 * @throws IdVacioException Excepción que se lanza cuando el id está vacío.
+	 * @throws FechaFormatoException Excepción que se lanza cuando la fecha se introduce en un formato incorrecto.
+	 * @throws SQLException Excepción que se lanza cuando hay errores relacionados con la base de datos.
+	 * @throws MotivoVacioException Excepción que se lanza cuando no se selecciona ningún motivo.
+	 * @throws DescripcionVaciaExceptioon Excepción que se lanza cuando no se introduce una descripción en los cuidados del animal.
+	 * @throws CampoVacioException Excepción que se lanza cuando hay un campo vacío.
+	 * @throws GeneroVacioException Excepción que se lanza cuando no se selecciona ningún género.
+	 * @throws DescripcionVaciaException Excepción que se lanza cuando no se introduce ninguna descripción en los tratados del animal.
+	 */
 	public Reptil (String nombre, LocalDate fechaNacimiento, MotivoAlta motivoAlta, LocalDate fechaAlta, boolean tipoPiel,
 			String cuidados) throws NombreVacioException, NombreInvalidoException, FechaFormatoException, SQLException, MotivoVacioException, DescripcionVaciaException {
 		super(nombre, fechaNacimiento, motivoAlta, fechaAlta, cuidados);
@@ -48,7 +97,26 @@ public class Reptil extends Animal{
 				
 	}	
 
-	//CONSTRUCTOR BAJA
+	/**
+	 * 	Constructor de la clase Reptil que extiende de Animal, utilizado para dar de baja a un Reptil.
+	 * 
+	 * @param Variable numérica de tipo short que contiene el id del animal.    
+	 * @param nombre Cadena de caracteres que contiene el nombre del animal.                         
+	 * @param fechaNacimiento Variable de tipo LocalDate que contiene la fecha de nacimiento del animal.
+	 * @param motivoBaja Proviene del Enum MotivoBaja que contiene los motivos de baja del animal.
+	 * @param fechaBaja De tipo LocalDate, contiene la fecha de baja del animal en el centro.
+	 * @param cuidados Cadena de caracteres que contiene una descripción del cuidado 
+	 * 				   necesario para el animal, en el caso de que lo necesite.
+	 * @param tipoPiel Variable de tipo boolean que indica el tipo de piel del reptil, escamas o caparazaón, siendo los valores true o false.   
+	 * @param cuidados Cadena de caracteres que contiene una descripción del cuidado 
+	 * 				   necesario para el animal, en el caso de que lo necesite.               
+	 * @throws NombreVacioException Excepción que se lanza cuando el nombre está vacío. 
+	 * @throws NombreInvalidoException Excepción que se lanza cuando el nombre no es válido.
+	 * @throws FechaFormatoException Excepción que se lanza cuando la fecha se introduce en un formato incorrecto.
+	 * @throws SQLException Excepción que se lanza cuando hay errores relacionados con la base de datos.
+	 * @throws MotivoVacioException Excepción que se lanza cuando no se selecciona ningún motivo.
+	 
+	 */	
 	public Reptil (short id,String nombre, LocalDate fechaNacimiento, MotivoBaja motivoBaja, LocalDate fechaBaja, boolean tipoPiel,
 			String cuidados) throws NombreVacioException, NombreInvalidoException, FechaFormatoException, SQLException, MotivoVacioException {
 		super(id, nombre, fechaNacimiento, motivoBaja, fechaBaja, cuidados);
@@ -66,11 +134,17 @@ public class Reptil extends Animal{
 	}
 
 
-
+	/**
+	 * Función que devuelve el tipo de piel del reptil.
+	 * @return Tipo de piel del reptil.
+	 */
 	public boolean getTipoPiel() {
 		return tipoPiel;
 	}
-
+	/**
+	 * Función que establece un tipo de piel en el objeto.
+	 * @param tipoPiel Tipo de piel.
+	 */
 	public void setTipoPiel(boolean tipoPiel) {
 		this.tipoPiel = tipoPiel;
 	}
--- ProyectoFinal_AgustinArcos/src/main/java/Clases/Usuario.java
+++ ProyectoFinal_AgustinArcos/src/main/java/Clases/Usuario.java
@@ -14,16 +14,40 @@ import Excepciones.ContrasenaVaciaException;
 import Excepciones.FechaFormatoException;
 import Excepciones.IdInvalidoException;
 import Excepciones.IdVacioException;
+import Excepciones.MotivoVacioException;
 import Excepciones.NombreInvalidoException;
 import Excepciones.NombreVacioException;
 import Superclases.EntidadConIdFechaYNombre;
 import Utils.UtilsDB;
-
+/**
+ * Clase que contiene un objeto de tipo Usuario que extiende de la superclase EntidadConIdFechaYNombre.
+ * @author Agustín Arcos
+ *
+ */
 public class Usuario extends EntidadConIdFechaYNombre {
-
+	/**
+	 * Variable de tipo String que indica el apellido del usuario.
+	 */
 	private String apellidos;
+	/**
+	 * Variable de tipo String que indica la contraseña del usuario.
+	 */
 	private String contrasena;
-
+/**
+ * Constructor de la clase Usuario.
+ * 
+ * @param nombre Cadena de caracteres que contiene el nombre del usuario.
+ * @param apellidos Variable de tipo String que indica la contraseña del usuario.
+ * @param id Variable numérica de tipo short que contiene la id del usuario. 
+ * @param contrasena Variable de tipo String que indica la contraseña del usuario.
+ * @param fechaNacimiento Variable de tipo LocalDate que contiene la fecha de nacimiento del animal.
+ * @throws ContrasenaVaciaException Excepción que se lanza cuando el campo contraseña está vacío.
+ * @throws ContrasenaLargaException Excepción que se lanza cuando la contraseña es más larga a lo establecido.
+ * @throws ContrasenaInvalidaException Excepción que se lanza cuando la contrasñea introducida no es válida.
+ * @throws NombreInvalidoException Excepción que se lanza cuando el nombre introducido no es válido.
+ * @throws NombreVacioException Excepción que se lanza cuando el campo del nombre está vacío.
+ * @throws FechaFormatoException Excepción que se lanza cuando el formato de fecha introducido no es correcto.
+ */
 	public Usuario(String nombre, String apellidos, short id, String contrasena, LocalDate fechaNacimiento)
 			throws ContrasenaVaciaException, ContrasenaLargaException, ContrasenaInvalidaException,
 			NombreInvalidoException, NombreVacioException, FechaFormatoException {
@@ -32,16 +56,30 @@ public class Usuario extends EntidadConIdFechaYNombre {
 		this.setContrasena(contrasena);
 	}
 
-//Insertar usuario en la base de datos.
+	/**
+	 * Constructor de la clase Usuario, para insertar un usuario en la base de datos.
+	 * 
+	 * @param nombre Cadena de caracteres que contiene el nombre del usuario.
+	 * @param apellidos Variable de tipo String que indica la contraseña del usuario.
+	 * @param contrasena Variable de tipo String que indica la contraseña del usuario.
+	 * @param fechaNacimiento Variable de tipo LocalDate que contiene la fecha de nacimiento del animal.
+	 * @throws ContrasenaVaciaException Excepción que se lanza cuando el campo contraseña está vacío.
+	 * @throws ContrasenaLargaException Excepción que se lanza cuando la contraseña es más larga a lo establecido.
+	 * @throws ContrasenaInvalidaException Excepción que se lanza cuando la contrasñea introducida no es válida.
+	 * @throws NombreInvalidoException Excepción que se lanza cuando el nombre introducido no es válido.
+	 * @throws NombreVacioException Excepción que se lanza cuando el campo del nombre está vacío.
+	 * @throws FechaFormatoException Excepción que se lanza cuando el formato de fecha introducido no es correcto.
+	 * @throws SQLException Excepción que se lanza cuando hay algún problema relacionado con la base de datos.
+	 */
 	public Usuario(String nombre, String apellidos, String contrasena, LocalDate fechaNacimiento)
 			throws NombreVacioException, NombreInvalidoException, SQLException, FechaFormatoException, ContrasenaVaciaException, ContrasenaInvalidaException, ContrasenaLargaException {
 		super(fechaNacimiento, nombre);
-
+		//Conexión a la base de datos.
 		Statement query = UtilsDB.conectarBD();
 
 		
 		
-		
+		//Inserción de los datos del usuario en la base de datos.
 		if (query.executeUpdate("insert into usuarios values(null,'" + nombre + "','" + apellidos + "','"
 				+ fechaNacimiento + "','" + contrasena + "'" + ")") > 0)
 
@@ -52,9 +90,22 @@ public class Usuario extends EntidadConIdFechaYNombre {
 		} else {
 			throw new SQLException("No se ha podido insertar el usuario");
 		}
+		//Desconexión de la base de datos.
 		UtilsDB.desconectarBD();
 	}
-
+	/**
+	 * Constructor de la clase Usuario, para hacer login del usuario.
+	 * 
+	 * @param nombre Cadena de caracteres que contiene el nombre del usuario.
+	 * @param contrasena Variable de tipo String que indica la contraseña del usuario.
+	 * @throws ContrasenaVaciaException Excepción que se lanza cuando el campo contraseña está vacío.
+	 * @throws ContrasenaLargaException Excepción que se lanza cuando la contraseña es más larga a lo establecido.
+	 * @throws ContrasenaInvalidaException Excepción que se lanza cuando la contrasñea introducida no es válida.
+	 * @throws NombreInvalidoException Excepción que se lanza cuando el nombre introducido no es válido.
+	 * @throws NombreVacioException Excepción que se lanza cuando el campo del nombre está vacío.
+	 * @throws FechaFormatoException Excepción que se lanza cuando el formato de fecha introducido no es correcto.
+	 * @throws SQLException Exceptión que se lanza cuando hay algún problema relacionado con la base de datos.
+	 */
 	public Usuario(String nombre, String contrasena)
 			throws NombreVacioException, NombreInvalidoException, SQLException, ContrasenaInvalidaException, ContrasenaVaciaException, ContrasenaLargaException {
 		super(nombre);
@@ -78,19 +129,34 @@ public class Usuario extends EntidadConIdFechaYNombre {
 		}
 		UtilsDB.desconectarBD();
 	}
-
+	/**
+	 * Función que devuelve los apellidos del usuario.
+	 * @return Apellidos del usuario.
+	 */
 	public String getApellidos() {
 		return apellidos;
 	}
-
+	/**
+	 * Función que establece apellidos al objeto.
+	 * @param apellidos Los apellidos del usuario.
+	 */
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-
+	/**
+	 * Función que devuelve la contraseña.
+	 * @return Contraseña.
+	 */
 	public String getContrasena() {
 		return contrasena;
 	}
-
+	/**
+	 * Función que establece una contraseña en el objeto.
+	 * @param contraseña La contraseña del usuario.
+	 * @throws ContrasenaInavalidaException Excepción que se lanza cuando la contraseña introducida no es válida.
+	 * @throws ContrasenaLargaException Excepción que se lanza cuando el tamaño de la contrasela es más larga de lo establecido.
+	 * @throws ContrasenaVaciaExceptiopn Excepción que se lanza cuando el campo de la contraseña está vacío.
+	 */
 	public void setContrasena(String contrasena)
 			throws ContrasenaInvalidaException, ContrasenaLargaException, ContrasenaVaciaException {
 		if(contrasena.isEmpty() ) {
