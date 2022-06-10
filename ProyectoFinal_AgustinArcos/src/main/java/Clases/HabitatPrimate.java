82438b9662b9e706cf7cf355a0ca037ab4c4c666
nostromo97 <chesopan@gmail.com>
2022-06-10 19:19:38 +0200

    documentación todos los habitat
    documentación todos los habitat

--- ProyectoFinal_AgustinArcos/src/main/java/Clases/HabitatPrimate.java
+++ ProyectoFinal_AgustinArcos/src/main/java/Clases/HabitatPrimate.java
@@ -1,18 +1,35 @@
 package Clases;
-
+/**
+ * Clase que contiene un objeto de tipo HabitatPrimate que extiende de la clase Habitat.
+ * @author Agustín Arcos
+ *
+ */
 public class HabitatPrimate extends Habitat{
-
+	/**
+	 * Variable numérica de tipo short que indica la cantidad de árboles en el habitat.
+	 */
 	private short cantidadArboles;
-
+/**
+ * Constructor de la clase HabitatPrimate
+ * @param nombre Cadena de caracteres que contiene el nombre del animal.
+ * @param tipo Indica el tipo de habitat.
+ * @param cantidadArboles Variable numérica de tipo short que indica la cantidad de árboles en el habitat.
+ */
 	public HabitatPrimate(String nombre, String tipo, short cantidadArboles) {
 		super(nombre, tipo);
 		this.cantidadArboles = cantidadArboles;
 	}
-
+	/**
+	 * Función que devuelve la cantidad de árboles
+	 * @return Cantidad de árboles del objeto.
+	 */
 	public short getCantidadArboles() {
 		return cantidadArboles;
 	}
-
+	/**
+	 * Función que establece una cantidad de árboles.
+	 * @param cantidadArboles Cantidad de árboles.
+	 */
 	public void setCantidadArboles(short cantidadArboles) {
 		this.cantidadArboles = cantidadArboles;
 	}
--- ProyectoFinal_AgustinArcos/src/main/java/Clases/HabitatReptil.java
+++ ProyectoFinal_AgustinArcos/src/main/java/Clases/HabitatReptil.java
@@ -1,18 +1,36 @@
 package Clases;
-
-public class HabitatReptil {
-
+/**
+ * Clase que contiene un objeto de tipo HabitatReptil que extiende de la clase Habitat.
+ * @author Agustín Arcos
+ *
+ */
+public class HabitatReptil extends Habitat{
+	
+	/**
+	 * Variable numérica de tipo short que indica el tamaño del terreno de arena.
+	 */
 	private short terrenoArena;
-
-	public HabitatReptil(short terrenoArena) {
-		super();
+	/**
+	 * Constructor de la clase HabitatReptil.
+	 * @param nombre Cadena de caracteres que contiene el nombre del animal.
+	 * @param tipo Cadena de caracteres que contiene el tipo de habitat.
+	 * @param terrenoArena Variable numérica de tipo short que indica el tamaño del terreno de arena.
+	 */
+	public HabitatReptil(String nombre, String tipo, short terrenoArena) {
+		super(nombre, tipo);
 		this.terrenoArena = terrenoArena;
 	}
-
+	/**
+	 * Función que devuelve el tamaño del terreno de arena.
+	 * @return Tamaño del terreno de arena.
+	 */
 	public short getTerrenoArena() {
 		return terrenoArena;
 	}
-
+	/**
+	 * Función que establece un tamaño al terreno de arena del objeto.
+	 * @param terrenoArena Tamaño del terreno de arena.
+	 */
 	public void setTerrenoArena(short terrenoArena) {
 		this.terrenoArena = terrenoArena;
 	}
