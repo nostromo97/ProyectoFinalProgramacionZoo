87d55e05c682524d4c37769d71b1ef5c5910a98b
nostromo97 <chesopan@gmail.com>
2022-06-10 19:11:46 +0200

    documentacion de habitat y habitat anfibio
    documentacion de habitat y habitat anfibio

--- ProyectoFinal_AgustinArcos/src/main/java/Clases/Habitat.java
+++ ProyectoFinal_AgustinArcos/src/main/java/Clases/Habitat.java
@@ -1,20 +1,36 @@
 package Clases;
 
 import Superclases.EntidadConNombre;
-
+/**
+ * Clase que contiene un objeto de tipo Habitat que extiende de la superclase EntidadConNombre.
+ * @author Agustín Arcos
+ *
+ */
 public class Habitat extends EntidadConNombre{
-
+	/**
+	 * Cadena de caracteres que contiene el nombre del habitat.
+	 */
 	private String tipo;
-
+/**
+ * Constructor de la clase Habitat.
+ * @param nombre Cadena de caracteres que contiene el nombre del habitat.
+ * @param tipo Cadena de caracteres que contiene el tipo de habitat.
+ */
 	public Habitat(String nombre, String tipo) {
 		super(nombre);
 		this.tipo = tipo;
 	}
-
+	/**
+	 * Función que devuelve el tipo de habitat
+	 * @return tipo de habitat del objeto
+	 */
 	public String getTipo() {
 		return tipo;
 	}
-
+	/**
+	 * Función que establece un tipo de habitat al objeto.
+	 * @param tipo Tipo de habitat.
+	 */
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
--- ProyectoFinal_AgustinArcos/src/main/java/Clases/HabitatAnfibio.java
+++ ProyectoFinal_AgustinArcos/src/main/java/Clases/HabitatAnfibio.java
@@ -1,20 +1,37 @@
 package Clases;
 
 
-
+/**
+ * Clase que contiene un objeto de tipo HabitatAnfibio que extiende de la clase Habitat.
+ * @author Agustín Arcos
+ *
+ */
 public class HabitatAnfibio extends Habitat{
-
+	/**
+	 * Variable numérica de tipo short que indica los metros cubicos de agua en el habitat.
+	 */
 	private short metrosCubicosAgua;
-
+/**
+ * Constructor de HabitatAnfibio
+ * @param nombre Cadena de caracteres que contiene el nombre del habitat.
+ * @param tipo Cadena de caracteres que indica el tipo de habitat.
+ * @param metrosCubicosAgua Variable numérica de tipo short que indica los metros cubicos de agua en el habitat.
+ */
 	public HabitatAnfibio(String nombre, String tipo, short metrosCubicosAgua) {
 		super(nombre, tipo);
 		this.metrosCubicosAgua = metrosCubicosAgua;
 	}
-
+	/**
+	 * Función que devuelve los metro cubicos de agua del habitat.
+	 * @return Metros cúbicos de agua del habitat.
+	 */
 	public short getMetrosCubicosAgua() {
 		return metrosCubicosAgua;
 	}
-
+	/**
+	 * Función que establece los metros cubicos de agua del habitat
+	 * @param metrosCubicosAgua Los metros cubicos de agua.
+	 */
 	public void setMetrosCubicosAgua(short metrosCubicosAgua) {
 		this.metrosCubicosAgua = metrosCubicosAgua;
 	}
